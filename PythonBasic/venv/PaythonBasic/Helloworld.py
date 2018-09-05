60ffad04a2f30a347fcf311caa015cf2ffb8a87e
scnulc <lc1234563919512>
2018-09-05 22:57:28 +0800

    Add some calculation

---
+++ PythonBasic/venv/PaythonBasic/Helloworld.py
@@ -0,0 +1,19 @@
+# This program says hello and ask my name
+
+print('hello world!')
+
+print('what''s your name?')
+
+myName = input();
+
+print('It is good to meet you ' + myName)
+
+print('The length of your name is ')
+
+print(len(myName))
+
+print('What''s your age? ')
+
+myAge = input()
+
+print('you will be ' + str(int(myAge) + 1) + ' in a year')
---
+++ PythonBasic/venv/PaythonBasic/PythonBasicGrammer.py
@@ -0,0 +1,11 @@
+# print(2+3+6)
+
+
+# print(8/4)
+
+# 'Hello
+
+# print('Alice' * 5)
+
+# print('Alice' * 'i')
+print('Alice' *  5.0)
